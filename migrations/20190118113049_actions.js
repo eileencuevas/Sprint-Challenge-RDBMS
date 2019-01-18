518789dfc19a179513ca9f05d79705a8d5403f7f
steelworx <eileencuevas62@gmail.com>
2019-01-18 12:16:36 -0500

    added column for project_id in actions table

--- migrations/20190118113049_actions.js
+++ migrations/20190118113049_actions.js
@@ -2,11 +2,16 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('actions', table => {
       table.increments();
-      table.string('description', 255);
-      table.string('notes', 1000);
+      table.string('description', 255).notNullable();
+      table.string('notes', 1000).defaultTo('none');
       table
         .boolean('isComplete')
         .defaultTo(false);
+      table
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('projects.id');
   })
 };
 
