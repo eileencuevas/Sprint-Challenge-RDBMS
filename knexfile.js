b38ea01241251c6d8398ef1bc02019a2625293b4
steelworx <eileencuevas62@gmail.com>
2019-01-18 11:25:16 -0500

    changed filename in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './projectsActions.sqlite3'
     },
     useNullAsDefault: true,
   }
